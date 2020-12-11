0467b50ce8966f070fc88cdacfdfb10b9d51be62
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-12-11 03:06:52 +0000

    updated personal info

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,37 +1,48 @@
 import React, { Component }  from 'react';
+import Swiper from 'react-id-swiper';
 import {Card} from 'react-bootstrap';
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import "../Home/Home.css";
 import Divider from "@material-ui/core/Divider";
-import Info from "../../info.json";
+import Infos from "../../info";
+
 
 export default function Portfolio() {
+const selectPic = (key)=>{
 
+}
 return(
     <>    
     <p className="aboutMeH4">Portfolio</p>
     <Divider style={{ border: '2px solid cornflowerblue',
     backgroundColor: 'cornflowerblue' }}/>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="info[0].images" />
+    {Infos.map((info)=>(
+<Card style={{ width: '18rem', display:"inline-flex"}} key={info.id}>
+  <Card.Img alt="gif" src={info.images[0]} type="video/mp4" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>{info.name}</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+      {info.Decription}
     </Card.Text>
   </Card.Body>
   <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+    <ListGroupItem>{info.tech}</ListGroupItem>
+    <ListGroupItem>{info.tech}</ListGroupItem>
     <ListGroupItem>Vestibulum at eros</ListGroupItem>
   </ListGroup>
   <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
+    <Card.Link href={info.repo}>GitHub</Card.Link>
+    <Card.Link href={info.deployed}>Deployed</Card.Link>
   </Card.Body>
 </Card>
-
+  
+  ))}
+  {/* <Swiper>
+    <div src={info.images[0]}></div>
+    <div src={info.images[1]}></div>
+    <div src={info.images[2]}></div>
+    <div src={info.images[3]}></div>
+  </Swiper> */}
 </>
 )
 }
